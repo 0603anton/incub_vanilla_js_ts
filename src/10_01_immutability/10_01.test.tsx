b2979a965b9e47744f217c5b3c9121c64039b599
0603anton <zorgzb@gmail.com>
2022-12-06 09:39:59 +0200

    native 10- 1:23

--- src/10_01_immutability/10_01.test.tsx
+++ src/10_01_immutability/10_01.test.tsx
@@ -1,15 +1,20 @@
-import {UserType} from "./10_01";
+import {
+    addBook, addCompany,
+    change_adr, changeCompany, changeCompany2,
+    makeHair,
+    moveUserToOtherHouse, removeBook,
+    update_laptop, updateBook,
+    UserType,
+    UserWithBooksType,
+    UserWithLaptopType, WithCompaniesType
+} from "./10_01";
 
-function makeHair(u:UserType){
-    const userCopy= {...u, hair: u.hair/2}
-    return userCopy
-}
 
-test('reference type function, immutability works', ()=>{
-    let user:UserType={
-        name:'Anton',
-        hair:32,
-        address:{title:'Novokuzntesk'}
+test('reference type function, immutability works', () => {
+    let user: UserType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Novokuzntesk'}
     }
 
     const hairedUser = makeHair(user)
@@ -17,4 +22,202 @@ test('reference type function, immutability works', ()=>{
     expect(user.hair).toBe(32)
     expect(hairedUser.hair).toBe(16)
     expect(hairedUser.address).toBe(user.address) // подтверждение поверхностной копии
+})
+
+
+test('change address', () => {
+    let awesomeUser: UserWithLaptopType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`}
+    }
+
+    const movedUser = change_adr(awesomeUser)
+
+    expect(awesomeUser).not.toBe(movedUser)
+    expect(movedUser.laptop).toBe(awesomeUser.laptop)
+    expect(movedUser.address).not.toBe(awesomeUser.address) // подтверждение поверхностной копии
+})
+
+
+test('Upgrade user laptop', () => {
+    let awesomeUser: UserWithLaptopType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`}
+    }
+
+    const upgraded = update_laptop(awesomeUser)
+
+    expect(awesomeUser).not.toBe(upgraded)
+    expect(upgraded.laptop).not.toBe(awesomeUser.laptop)
+    expect(upgraded.laptop.title).toBe(`MacBook`)
+    expect(upgraded.address).toBe(awesomeUser.address) // подтверждение поверхностной копии
+})
+
+test('Upgrade user adresse', () => {
+    let awesomeUser: UserWithLaptopType & UserWithBooksType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`},
+        books: [`harry potter1`, `harry potter2`]
+    }
+
+    const upgraded = moveUserToOtherHouse(awesomeUser)
+
+    expect(awesomeUser).not.toBe(upgraded)
+    expect(awesomeUser.books).toBe(upgraded.books)
+    expect(upgraded.laptop).toBe(awesomeUser.laptop)
+    expect(upgraded.address).not.toBe(awesomeUser.address)
+    expect(upgraded.address.title).not.toBe(awesomeUser.address.title)
+})
+
+
+test('add book', () => {
+    let awesomeUser: UserWithLaptopType & UserWithBooksType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`},
+        books: [`harry potter1`, `harry potter2`]
+    }
+
+    const upgraded = addBook(awesomeUser)
+
+    expect(awesomeUser).not.toBe(upgraded)
+    expect(awesomeUser.books).not.toBe(upgraded.books)
+    expect(upgraded.books[2]).toBe(`harry potter 3`)
+    expect(upgraded.laptop).toBe(awesomeUser.laptop)
+    expect(upgraded.address).toBe(awesomeUser.address)
+    expect(upgraded.address.title).toBe(awesomeUser.address.title)
+})
+
+test('updateNewBook', () => {
+    let awesomeUser: UserWithLaptopType & UserWithBooksType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`},
+        books: [`js`, `css`, 'hz']
+    }
+
+    const upgraded = updateBook(awesomeUser, `js`, `ts`)
+
+    expect(awesomeUser).not.toBe(upgraded)
+    expect(awesomeUser.books).not.toBe(upgraded.books)
+    expect(upgraded.books[0]).toBe(`ts`)
+    expect(upgraded.laptop).toBe(awesomeUser.laptop)
+    expect(upgraded.address).toBe(awesomeUser.address)
+    expect(upgraded.address.title).toBe(awesomeUser.address.title)
+})
+
+test('removeBook', () => {
+    let awesomeUser: UserWithLaptopType & UserWithBooksType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`},
+        books: [`js`, `css`, 'hz']
+    }
+
+    const upgraded = removeBook(awesomeUser, `js`)
+
+    expect(awesomeUser).not.toBe(upgraded)
+    expect(awesomeUser.books).not.toBe(upgraded.books)
+    expect(upgraded.books.length).toBe(2)
+    expect(upgraded.books[0]).toBe('css')
+    expect(upgraded.laptop).toBe(awesomeUser.laptop)
+    expect(upgraded.address).toBe(awesomeUser.address)
+    expect(upgraded.address.title).toBe(awesomeUser.address.title)
+})
+
+test('addCompany', () => {
+    let awesomeUser: UserWithLaptopType & WithCompaniesType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`},
+        companies: [
+            {
+                id: 1,
+                title: `Sber`
+            },
+            {
+                id: 2,
+                title: `it-incubator`
+            },
+        ]
+    }
+
+
+    const upgraded = addCompany(awesomeUser, `google`)
+
+    expect(awesomeUser).not.toBe(upgraded)
+    expect(upgraded.laptop).toBe(awesomeUser.laptop)
+    expect(upgraded.address).toBe(awesomeUser.address)
+    expect(upgraded.address.title).toBe(awesomeUser.address.title)
+    expect(upgraded.companies[2].title).toBe('google')
+})
+
+test('changeCompany', () => {
+    let awesomeUser: UserWithLaptopType & WithCompaniesType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`},
+        companies: [
+            {
+                id: 1,
+                title: `Sber`
+            },
+            {
+                id: 2,
+                title: `it-incubator`
+            },
+        ]
+    }
+
+
+    const upgraded = changeCompany(awesomeUser, 1, `google`)
+
+    expect(awesomeUser).not.toBe(upgraded)
+    expect(upgraded.laptop).toBe(awesomeUser.laptop)
+    expect(upgraded.address).toBe(awesomeUser.address)
+    expect(upgraded.address.title).toBe(awesomeUser.address.title)
+    expect(upgraded.companies[0].title).toBe('google')
+})
+
+test('changeCompany', () => {
+    let awesomeUser: UserWithLaptopType = {
+        name: 'Anton',
+        hair: 32,
+        address: {title: 'Dubovaya', city: 'Kalik'},
+        laptop: {title: `asus`},
+        companies:
+    }
+
+    let companies = {
+        'Anton': [
+            {
+                id: 1,
+                title: `Sber`
+            },
+            {
+                id: 2,
+                title: `it-incubator`
+            },
+        ],
+        'ARTEM': [
+            {
+                id: 1,
+                title: `Sber`
+            },
+
+        ]
+    }
+    changeCompany2(companies, 'Anton',1,'SBER')
+
 })
--- src/10_01_immutability/10_01.tsx
+++ src/10_01_immutability/10_01.tsx
@@ -1,5 +1,70 @@
-export type UserType ={
-    name:string
-    hair:number
-    address: {title:string}
+export type UserType = {
+    name: string
+    hair: number
+    address: { title: string, city?: string }
+}
+export type LaptopType = {
+    title: string
+}
+
+export type UserWithLaptopType = UserType & { //TODO новая фишка совмещения типов типа все св-ва этого типа + лэптоп
+    laptop: LaptopType
+}
+
+export type UserWithBooksType = UserWithLaptopType & {
+    books: string[]
+}
+
+export type WithCompaniesType = {
+    companies: Array<{id:number, title:string }>
+}
+
+
+
+export function makeHair(u: UserType) {
+    const userCopy = {...u, hair: u.hair / 2}
+    return userCopy
+}
+
+export function change_adr(u: UserWithLaptopType) {
+    return {...u, address: {...u.address, city: `Moscow`}}
+}
+
+export const update_laptop = (u: UserWithLaptopType) => {
+    return {...u, laptop: {...u.laptop, title: `MacBook`}}
+}
+
+export function moveUserToOtherHouse(u: UserWithLaptopType & UserWithBooksType) {
+    return {...u, address: {...u.address, title: `Izumrudnaya`}}
+}
+
+export function addBook(u: UserWithLaptopType & UserWithBooksType) {
+    return {...u, books: [...u.books, `harry potter 3`]}
+}
+
+export function updateBook(u: UserWithLaptopType & UserWithBooksType, oldBook: string, newBook: string) {
+    return {...u, books: u.books.map((book) => book === oldBook ? newBook : book)}
+
+    // Ошибся в том, что положил обратно в ведро всё, а именно, сразу обернул в массив
+    // и пришлось ещё дополнительно деструктурировать, а это лишнее. Второе лишнее, то что я зачем, то присваивал
+    // бук значение и его возвращал, это вообще непонятно зачем. Map же возвращает элементы поочередно. Ошибки ниже:
+    // {...u, books:[...u.books.map((book)=> book === oldBook? book=newBook:book)]}
+}
+
+export function removeBook(u: UserWithLaptopType & UserWithBooksType, bookToRemove: string) {
+    return {...u, books: u.books.filter((book)=> book !== bookToRemove)}
+}
+
+export function addCompany(u: UserWithLaptopType & WithCompaniesType, company: string) {
+    return {...u, companies: [...u.companies, {id:3, title:company}]}
+}
+
+export function changeCompany(u: UserWithLaptopType & WithCompaniesType,id:number, company: string) {
+    return {...u, companies: u.companies.map((compan)=> compan.id === id ? {...compan, title:company} : compan)}
+}
+// TODO в функцию достаточно, только частично передавать тип, где присутсвует св-во с которым работает функция
+
+export function changeCompany2(companies:{[key:string]:WithCompaniesType[]}, userName: string, comoanyID:number, newTitle:string) {
+    let copy = {...companies}
+    copy[userName] = copy['Anton'].map
 }
