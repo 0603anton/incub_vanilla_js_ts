9ac5c9f9ccf581223a152045427d90b9be45f54e
0603anton <zorgzb@gmail.com>
2023-01-10 16:04:03 +0200

    native  вторник 4 неделя 10/01

--- src/11_вторник_4_неделя/lesson_8/lesson_8.ts
+++ src/11_вторник_4_неделя/lesson_8/lesson_8.ts
@@ -3,7 +3,7 @@
 
 export function sum(...nums: Array<number>): number {
     console.log(nums)
-    const result = nums.reduce((acc,el)=>{
+    const result = nums.reduce((acc, el) => {
         return acc + el;
     })
     //...здесь пишем код.
@@ -20,27 +20,30 @@ export function sum(...nums: Array<number>): number {
 //  - "11", если треугольник обычный,
 //  - "00", если такого треугольника не существует.
 
-export function getTriangleType(a: number,b: number,c: number): string {
-    if (a ==b && b == c){
-        return `10`
-    } else if ( a==b || b==c || c==a){
-        return `01`
-    } else if(a!==b || b!==c || c!==a){
-        return `11`
-    }// else if (a > (b+c) ||)
+export function getTriangleType(a: number, b: number, c: number): string {
+    if (a + b > c && a + c > b && b + c > a) {
+        if (a == b && b == c) {
+            return `10`
+        } else if (a == b || b == c || c == a) {
+            return `01`
+        } else if (a !== b || b !== c || c !== a) {
+            return `11`
+        }
+    }
     //...здесь пишем код.
     // В return стоит "заглушка", чтоб typescript не ругался
-    return ""
+    return "00"
 }
 
 
 // 3. Функция getSum принимает параметром целое число и возвращает
 // сумму цифр этого числа
 
-export function getSum(number: number): number{
+export function getSum(number: number): number {
+
     //...здесь пишем код.
     // В return стоит "заглушка", чтоб typescript не ругался
-    return 123
+    return number.toString().split(``).reduce((acc,el)=> acc+ Number(el),0)
 }
 
 
@@ -50,9 +53,19 @@ export function getSum(number: number): number{
 // В противном случае - false.
 
 export const isEvenIndexSumGreater = (arr: Array<number>): boolean => {
+let sumEven = arr.map((el,i)=>{
+    return i%2==0 ? el : 0
+}).reduce((acc, curr)=> {
+    return acc + curr
+})
+    let sumOdd = arr.map((el,i)=>{
+        return i%2!==0 ? el : 0
+    }).reduce((acc, curr)=> {
+        return acc + curr
+    })
     //...здесь пишем код.
     // В return стоит "заглушка", чтоб typescript не ругался
-    return true
+    return sumEven >sumOdd
 }
 
 // 5. Функция getSquarePositiveIntegers принимает параметром массив чисел и возвращает новый массив. 
